f1671db9110d429f4de0805cfdcf99ee281b0306
Vladislav <45949816+Rinjs@users.noreply.github.com>
2019-01-24 22:39:21 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,46 @@
+// Телефонная книга
+var phoneBook = {};
+
+/**
+ * @param {String} command
+ * @returns {*} - результат зависит от команды
+ */
+module.exports = function (command) {
+var commandArray=command.split(' ');
+switch (commandArray[0]) {
+    case 'ADD':add(commandArray);break;
+    case 'SHOW':return show();break;
+    case 'REMOVE_PHONE':return remove(commandArray);break;
+}
+
+function add(commandArr) {
+var name=commandArr[1];
+var number=commandArr[2].split(',');
+if(phoneBook.hasOwnProperty(name)){
+    phoneBook[name]=phoneBook[name].concat(number);
+}
+else phoneBook[name]= number;
+}
+function show() {
+        var res = [];
+        for (var name in phoneBook) {
+            res.push(name + ': ' + phoneBook[name].join(', '));
+        }
+    res.sort();
+        return res;
+}
+function remove(commandArr) {
+    var number=commandArr[1];
+    for (var key in phoneBook) {
+        var numberPos = phoneBook[key].indexOf(number);
+        if (numberPos != -1) {
+            phoneBook[key].splice(numberPos, 1);
+            if (phoneBook[key].length == 0) {
+                delete phoneBook[key];
+            }
+        return true;
+    }
+    }
+ return false;
+}
+};
